da3a853c85b0aaf62cd58e7e100114b4818b030c
Bluestar <adha9990@gmail.com>
2023-12-05 17:16:06 +0800

    [更新] 首頁影片更換

--- web/src/components/About_HSIPL.js
+++ web/src/components/About_HSIPL.js
@@ -237,10 +237,10 @@ export const About_HSIPL = () => {
                             <iframe
                                 width="100%"
                                 height="630"
-                                src="https://www.youtube.com/embed/Lh_4Z_jrwvk?controls=0"
+                                src="https://www.youtube.com/embed/NMExquRcMtA?si=mT2tFP4qw6YPyyF_"
                                 title="YouTube video player"
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowFullScreen
                             ></iframe>
                         </div>
